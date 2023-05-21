cd9b4abf5d68981075444d5046b2563a1fc97ecf
eduardoalmeida45 <eduardoalmeida5547@gmail.com>
2023-05-20 22:20:13 -0300

    lesson 02

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,11 +1,57 @@
+import { ImageBackground, Text, TouchableOpacity, View } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
-import { Text, View } from 'react-native';
+import { styled } from 'nativewind';
+
+import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
+import { BaiJamjuree_700Bold } from '@expo-google-fonts/bai-jamjuree';
+
+import blurBg from './src/assets/bg-blur.png';
+import Stripes from './src/assets/stripes.svg';
+import NLWLogo from './src/assets/nlw-spacetime-logo.svg';
+
+const StyledStripes = styled(Stripes);
 
 export default function App() {
+  const [hasLoadedFonts] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold,
+    BaiJamjuree_700Bold
+  });
+
+  if (!hasLoadedFonts) {
+    return null;
+  }
+
   return (
-    <View className="bg-gray-900 flex-1 items-center justify-center">
-      <Text className="text-gray-50 font-bold text-5xl">Hello World</Text>
+    <ImageBackground
+      source={blurBg}
+      imageStyle={{ position: 'absolute', left: '-100%' }}
+      className="bg-gray-900 px-8 py-10 flex-1 items-center relative"
+    >
+      <StyledStripes className="absolute left-2" />
+
+      <View className="flex-1 items-center justify-center gap-6">
+        <NLWLogo />
+
+        <View className="space-y-2">
+          <Text className="text-2xl text-gray-50 text-center font-title leading-tight">
+            Sua cápsula do tempo
+          </Text>
+          <Text className="text-base text-gray-100 text-center font-body leading-relaxed">
+            Colecione momentos marcantes da sua jornada e compartilhe (se quiser) com o mundo!
+          </Text>
+        </View>
+
+        <TouchableOpacity activeOpacity={0.7} className="bg-green-500 px-5 py-2 rounded-full">
+          <Text className="text-sm text-black uppercase font-alt">Cadastrar lembranças</Text>
+        </TouchableOpacity>
+      </View>
+
+      <Text className="text-sm text-gray-200 text-center font-body leading-relaxed">
+        Feito com 💜 no NLW da Rocketseat
+      </Text>
+
       <StatusBar style="light" translucent />
-    </View>
+    </ImageBackground>
   );
 }
--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,7 +1,70 @@
+import { User } from 'lucide-react';
+import Image from 'next/image';
+
+import nlwLogo from '../assets/nlw-spacetime-logo.svg';
+
 export default function Home() {
   return (
-    <div className="h-screen bg-zinc-900 text-zinc-50 p-6">
-      <h1 className="text-4xl font-bold">Sua cápsula do tempo</h1>
-    </div>
+    <main className="min-h-screen grid grid-cols-2">
+      <div className="px-28 py-16 bg-[url(../assets/bg-stars.svg)] bg-cover border-r border-white/10 flex flex-col items-start justify-between relative overflow-hidden">
+        <div className="h-[288px] w-[526px] bg-purple-700 rounded-full absolute right-0 top-1/2 -translate-y-1/2 translate-x-1/2 opacity-50 blur-full" />
+        <div className="w-2 bg-stripes absolute right-2 top-0 bottom-0" />
+
+        <a
+          href=""
+          className="text-left flex items-center gap-3 hover:text-gray-50 transition-colors"
+        >
+          <div className="h-10 w-10 bg-gray-400 rounded-full flex items-center justify-center">
+            <User className="h-5 w-5 text-gray-500" />
+          </div>
+
+          <p className="max-w-[140px] text-sm leading-snug">
+            <span className="underline">Crie sua conta</span> e salve suas memórias!
+          </p>
+        </a>
+
+        <div className="space-y-5">
+          <Image src={nlwLogo} alt="NLW Spacetime" />
+
+          <div className="max-w-[420px] space-y-1">
+            <h1 className="text-5xl text-gray-50 font-bold leading-tight">Sua cápsula do tempo</h1>
+            <p className="text-lg leading-relaxed">
+              Colecione momentos marcantes da sua jornada e compartilhe (se quiser) com o mundo!
+            </p>
+          </div>
+
+          <a
+            href=""
+            className="bg-green-500 text-black text-sm uppercase font-alt leading-none px-5 py-3 rounded-full inline-block hover:bg-green-600 transition-colors"
+          >
+            CADASTRAR LEMBRANÇA
+          </a>
+        </div>
+
+        <div className="text-gray-200 text-sm leading-relaxed">
+          Feito com 💜 no NLW da{' '}
+          <a
+            href="https://rocketseat.com.br"
+            target="_blank"
+            rel="moreferrer"
+            className="underline hover:text-gray-100 transition-colors"
+          >
+            Rocketseat
+          </a>
+        </div>
+      </div>
+
+      <div className="p-16 bg-[url(../assets/bg-stars.svg)] bg-cover flex flex-col">
+        <div className="flex flex-1 items-center justify-center">
+          <p className="w-[360px] text-center leading-relaxed">
+            Você ainda não registrou nenhuma lembrança, começe a{' '}
+            <a href="" className="underline hover:text-gray-50 transition-colors">
+              criar agora
+            </a>
+            !
+          </p>
+        </div>
+      </div>
+    </main>
   );
 }
